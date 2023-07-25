530c3380f7f6f51be536e311007596ec1c8425a3
Muhammad Burhan Ahmed <92679045+Burh2k@users.noreply.github.com>
2023-07-25 17:00:16 +0500

    Update 2.0

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
-var millitime = parseInt(document.getElementById("milli").textContent);
-var sectime = parseInt(document.getElementById("sec").textContent);
-var mintime = parseInt(document.getElementById("min").textContent);
-var hourtime = parseInt(document.getElementById("hour").textContent);
-var daytime = parseInt(document.getElementById("day").textContent);
+var currentDate = new Date();
+var newyear = new Date(currentDate.getFullYear() + 1, 0, 1);
+var timenew = newyear - currentDate;
+
+var counting;
+//Chaning the milliseconds to required time.
+var daytime = Math.floor(timenew / (1000 * 60 * 60 * 24));
+var hourtime = Math.floor((timenew % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var mintime = Math.floor((timenew % (1000 * 60 * 60)) / (1000 * 60));
+var sectime = Math.floor((timenew % (1000 * 60)) / 1000);
+
+document.getElementById('day').textContent = daytime;
+document.getElementById('hour').textContent = hourtime;
+document.getElementById('min').textContent = mintime;
+document.getElementById('sec').textContent = sectime;
+
 function count() {
-    if (millitime >= 0) {
-        document.getElementById("milli").textContent = millitime;
-        if (millitime == 0) {
-            millitime = 61;
-            sectime--;
-            document.getElementById("sec").textContent = sectime;
-        }
+    if (sectime >= 0) {
+        document.getElementById("sec").textContent = sectime;
         if (sectime == 0) {
-            millitime = 61;
-            sectime = 61;
+            sectime = 60;
             mintime--;
             document.getElementById("min").textContent = mintime;
         }
         if (mintime == 0) {
-            millitime = 61;
-            sectime = 61;
-            mintime = 61;
+            sectime = 60;
+            mintime = 60;
             hourtime--;
             document.getElementById("hour").textContent = hourtime;
         }
         if (hourtime == 0) {
-            millitime = 61;
-            sectime = 61;
-            mintime = 61;
+            sectime = 60;
+            mintime = 60;
             hourtime = 24;
             daytime--;
             document.getElementById("day").textContent = daytime;
         }
-        millitime--;
-        setTimeout(count, 18);
-    }
-}
-count();
-
-function change() {
-    var status = document.getElementById('stp').textContent;
-    if (status == stop) {
-        document.getElementById('stp').textContent = "Start";
+        sectime--;
+        counting = setTimeout(count, 1000);
     }
     else {
-        document.getElementById('stp').textContent = "Stop";
+        clearTimeout(counting);
     }
+
+}
+function change() {
+    if (document.getElementById('stp').textContent === "STOP") {
+        document.getElementById('stp').textContent = "START";
+        clearTimeout(counting);
+    }
+    else {
+        document.getElementById('stp').textContent = "STOP";
+        count();
+    }
+}
+function clc(){
+    document.getElementById("day").textContent = "360";
+    document.getElementById("hour").textContent = "24";
+    document.getElementById("min").textContent = "60";
+    document.getElementById("sec").textContent = "60";
 }
